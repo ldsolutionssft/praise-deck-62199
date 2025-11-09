2013bd6ff8b47c7a0457bdf1bd61819eb64236aa
LuisFlavioms <luisflavio628@gmail.com>
2025-11-09 17:02:21 -0300

    fix: first commit

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,5 +2,11 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
+}
+export function onBuy() {
+    if (window?.fbq) {
+        window.fbq("track", "InitiateCheckout");
+    }
+    window.location.href = "";
 }
